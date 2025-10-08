1e3d412ac4d67e9b102c8a45f75c8631a301e1b8
Rama Soni Ravictor <ramaravictor07@gmail.com>
2025-10-08 09:32:02 +0700

    pertemuan 2

---
+++ Tugas/Tugas2.md
@@ -0,0 +1,7 @@
+# Tugas 2
+
+Tampilan Endpoint Create Book:
+![Tampilan Endpoint Create](ss/endpoin_create.png)
+
+Tampilan endpoint update:
+![Tampilan Endpoint Create](ss/endpoin_create.png)
--- nim-node-server/server.js
+++ nim-node-server/server.js
@@ -1,16 +1,23 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
-const port = 3001;
+const PORT = 3001;
 
+// Middleware
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from Node.js Server!" });
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
+});
+
+const booksRouter = require("./routes/books");
+app.use("/api/books", booksRouter);
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
